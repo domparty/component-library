9113ce1bd6722a4a2b4dcaf2cca3b9a376409883
mjanssen <dev.marnix@gmail.com>
2020-04-11 17:33:21 +0200

    Use single quotes

--- src/components/Html/Html.tsx
+++ src/components/Html/Html.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode } from 'react';
 
 export interface HtmlProps {
   children: ReactNode;
@@ -6,11 +6,11 @@ export interface HtmlProps {
   comp?: string;
 }
 
-function Html({ className, children, comp: C = "div" }: HtmlProps) {
-  if (typeof children === "string") {
-    const tags = ["script", "style", "iframe"];
-    const regex = `<(${tags.join("|")})[^>]*>.*</(${tags.join("|")})>`;
-    children = children.replace(new RegExp(regex), "");
+function Html({ className, children, comp: C = 'div' }: HtmlProps) {
+  if (typeof children === 'string') {
+    const tags = ['script', 'style', 'iframe'];
+    const regex = `<(${tags.join('|')})[^>]*>.*</(${tags.join('|')})>`;
+    children = children.replace(new RegExp(regex), '');
   }
 
   return (
