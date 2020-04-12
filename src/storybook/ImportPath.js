360835accbe30615f5f7bdb8831c42ee255ac9f5
mjanssen <dev.marnix@gmail.com>
2020-04-12 23:24:17 +0200

    Updated import path

--- src/storybook/ImportPath.js
+++ src/storybook/ImportPath.js
@@ -4,5 +4,5 @@ export default function ImportPath({ data }) {
   if (typeof data.kind === 'undefined') return null;
 
   const exploded = data.kind.split(seperator);
-  return `${exploded[0]}/fe/${exploded[1].toLowerCase()}/${data.name}`;
+  return `import { ${data.name} } from ${exploded[0]}/fe/${exploded[1].toLowerCase()}`;
 }
