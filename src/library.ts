f1e56d468da3008b16eb14a947e6d7dfc9c8f434
mjanssen <dev.marnix@gmail.com>
2020-04-12 23:20:21 +0200

    Renamed directory names to lowercase

--- src/library.ts
+++ src/library.ts
@@ -1,2 +1,3 @@
+export { default as Grid } from './components/grid/Grid';
 export { default as Image } from './components/lazyImage/LazyImage';
-export { default as Html } from './components/Html/Html';
+export { default as Html } from './components/html/Html';
