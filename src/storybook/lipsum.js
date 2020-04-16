86c43b58f84c4cff8f19e654527f3d43251955fe
mjanssen <dev.marnix@gmail.com>
2020-04-16 15:19:12 +0200

    Added lipsum string

---
+++ src/storybook/lipsum.js
@@ -0,0 +1,9 @@
+export default `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam tristique nulla vel ipsum
+suscipit rutrum. Aliquam pulvinar lobortis purus, sed vestibulum arcu lacinia et.
+Pellentesque pulvinar lacus id tellus ullamcorper dignissim. Duis auctor justo nibh. Mauris
+interdum vitae lorem vel lobortis. Maecenas ornare dapibus ultrices. Nulla id ipsum
+vehicula, vestibulum erat varius, porta risus. Quisque vitae mattis ligula. Suspendisse
+auctor eros non cursus scelerisque. Maecenas dapibus risus ac vehicula euismod. Vestibulum
+lobortis blandit convallis. Suspendisse lobortis lacus a justo cursus, vitae fermentum ante
+faucibus. Nulla placerat, mi vel semper porttitor, massa nunc dapibus augue, et interdum est
+urna non leo.`;
